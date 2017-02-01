086a2b20df4f7596477be5c7c72c6c3013a87dda
Arthur Brainville <ybalrid@ybalrid.info>
2017-02-01 05:22:15 +0100

    add project, get network port

--- main.cpp
+++ main.cpp
@@ -1,6 +1,32 @@
 #include <iostream>
+#include <string>
+
+using namespace std;
+
+void pause(const string& message = "Press enter to continue...")
+{
+	cout << message << '\n';
+	cin.get();
+}
 
 int main(int argc, char* argv[])
 {
+	//Just for debug : display all the args!
+	for (auto i{ 0U }; i < argc; ++i)
+		cout << "arg : " << i << " : " << argv[i] << '\n';
+
+	auto port = 4242UL;
+
+	//If more than one arg, 2nd is the port number to listen to incoming TCP
+	if (argc > 1)
+		port = stoul(argv[1]);
+
+	cout << "Net port to use is : " << port << '\n';
+
+	//Start server here
+
+#ifdef _WIN32
+	pause();
+#endif
 	return 0;
 }
